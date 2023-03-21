a7dedd6ee9a172e8d0da84f1cdcfcd7ffe5c33f8
Stephane Plaisance <stephane.plaisance@vib.be>
2023-03-21 14:53:20 +0100

    Update run_elprep.sh

--- elprep5_calling/run_elprep.sh
+++ elprep5_calling/run_elprep.sh
@@ -18,9 +18,26 @@ dbsnp=${reffolder}/ens_dbSNP_150/GRCg6a_dbSNP_150.elsites
 
 pixeldist=2500
 
+# threads, adapt to your machine
+nthr=22
+
 workdir=/data/NC_projects/4120_ARuiz_chicken_variants
 cd ${workdir}
 mkdir -p tmpfiles elprep
+
+######################
+# Memory and TMP Files
+######################
+
+# handle tmpfiles as output for intermediate files if sfm mode
+#elprepmode=filter # all in RAM
+elprepmode=sfm # less in RAM
+
+if [ ${elprepmode} == "sfm" ]; then
+tmpopt="--tmp-path ${workdir}/tmpfiles"
+else
+tmpopt=""
+fi
 
 # run loop
 for inbam in bwa_mappings/*_rawmappings.bam; do
@@ -28,7 +45,7 @@ for inbam in bwa_mappings/*_rawmappings.bam; do
 pfx=$(basename ${inbam%_rawmappings.bam})
 outbam=${workdir}/elprep/${pfx}_elprep_mappings.bam
 
-elprep sfm ${inbam} ${outbam} \
+elprep ${elprepmode} ${inbam} ${outbam} \
   --optical-duplicates-pixel-distance ${pixeldist} \
   --mark-duplicates \
   --mark-optical-duplicates ${workdir}/elprep/${pfx}.output.metrics \
@@ -38,9 +55,9 @@ elprep sfm ${inbam} ${outbam} \
   --reference ${ref}  \
   --haplotypecaller ${workdir}/elprep/${pfx}.g.vcf.gz \
   --reference-confidence GVCF \
-  --nr-of-threads 80 \
+  --nr-of-threads ${nthr} \
   --timed \
   --log-path ${workdir}/elprep \
-  --tmp-path ${workdir}/tmpfiles
+  ${tmpopt}
 
 done
